1d8876ddc8355b7881d959dcb5f0b126bbae6021
ardatoprak <ardattoprak@gmail.com>
2023-01-23 15:05:52 +0300

    UIView.animated wrote.

--- Practical Animation/ViewController.swift
+++ Practical Animation/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var animatableView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /*
+        UIView.animate(withDuration: 2) {
+            self.animatableView.backgroundColor = .red
+            self.animatableView.frame = CGRect(x: 100, y: 300, width: 70, height: 40)
+        } */
+        
+        /*
+        UIView.animate(withDuration: 2) {
+            self.animatableView.backgroundColor = .red
+        } completion: { _ in
+            // Animasyon tamamlandıktan sonra çalışır.
+            UIView.animate(withDuration: 1) {
+                self.animatableView.frame = CGRect(x: 100, y: 300, width: 70, height: 40)
+            }
+        }*/
+        UIView.animate(withDuration: 2, delay: 3,options: [.repeat], animations: {
+            self.animatableView.backgroundColor = .red
+            self.animatableView.frame = CGRect(x: 100, y: 300, width: 70, height: 40)
+        }, completion: nil)
+    }
+        
 }
 
